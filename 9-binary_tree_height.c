75769c40f81d40595943125f6de828bfdffeb103
Njugi92 <kenkaruma.kk@gmail.com>
2023-08-30 04:45:13 +0300

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -21,6 +21,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			l = tree->left ? l + binary_tree_height(tree->left) : 0;
 			r = tree->right ? l + binary_tree_height(tree->right) : 0;
 		}
-		return ((l > r) ? l : r);
+		return ((l < r) ? l : r);
 	}
 }
